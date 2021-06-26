89f080dec29c786cdc7e192bd02a9ba460e6fe4e
zyy”git config --global user.name Yod <zhangyuan9980211@gmail.com>
2021-06-26 23:28:01 +0100

    structure of Bimads

--- src/BiMADS.jl
+++ src/BiMADS.jl
@@ -20,7 +20,6 @@ mutable struct B_points
     end
 end
 
-
 function s(x)
     return abs(floor(x + 1 / 2) - x)
 end
@@ -34,11 +33,10 @@ function f(x::Vector{Float64})
     return Taka
 end
 
-
 function test1(x)
     f1(x) = (x[1] + 2) .^ 2 + (x[2] - 2) .^ 2 - 10.0
     # f2(x) = f(x)
-    f2(x) = (x[1] + 45) .^ 2 + (x[2] - 1) .^ 2 -20
+    f2(x) = (x[1] + 20) .^ 2 + (x[2] - 1) .^ 2 - 20
     return f1, f2
 end
 
@@ -46,26 +44,8 @@ function ex005(x)
     return [x[1]^2 - x[2]^2; x[1] / x[2]]
 end
 
-# obj(x) = x[1]^2
-
 p = DSProblem(2; objective = test1, initial_point = [0, 0], full_output = false);
 
-
-# SetVariableRange(p,1,0.,0.19)
-# cons1(x) = x[1] > 0.
-# AddExtremeConstraint(p, cons1)
-# cons2(x) = x[1] <0.25
-# AddExtremeConstraint(p, cons2)
-
-# Optimize!(p)
-# OutputIterationDetails(p)
-"""
-Test
-"""
-function testbi(p::DSProblem)
-    println("recompilesss")
-    OutputIterationDetails(p)
-end
 
 """
     p_split(p::DSProblem)::Tuple{DSProblem,DSProblem}
@@ -132,7 +112,6 @@ end
 find the Pareto points, only for this BiMADS.jl, not general methods
 """
 function pareto_front(points::Vector{B_points}, minimize = true)
-    display(points)
     cmp_strict = minimize ? (<) : (>)
     cmp = minimize ? (<=) : (>=)
     dims = 2 #number of objective functions, needs modification for multi-MADS
@@ -155,48 +134,34 @@ end
 get the initial undominated points from p1 and p2
 """
 function initial_X_L(p1, p2)::Vector{B_points}
-    # ref_point = [0.0, 0.0]
-    # initial_point = [0.0, 0.0]
     x_L = Vector{B_points}()
     # optmization for p1
     append!(x_L, get_Bpoints(p1, p2, 1))
     x_L = reverse(x_L)
     # optmization for p2
     append!(x_L, get_Bpoints(p2, p1, 2))
-    # pareto_set = pareto_front(pareto_set)
-
-    # max_distance(p1,p2) = LinearAlgebra.norm(pareto_set[i].cost - pareto_set[i-1].cost)
-    # +LinearAlgebra.norm(pareto_set[i].cost - pareto_set[i+1].cost)
-    # max_index = 2
-
-
-
-
-
-
-    # for i = 2:length(pareto_set)-1
-    #     dis = LinearAlgebra.norm(pareto_set[i].cost - pareto_set[i-1].cost)
-    #     +LinearAlgebra.norm(pareto_set[i].cost - pareto_set[i+1].cost)
-    #     if dis >= max_distance
-    #         max_distance = dis
-    #         max_index = i
-    #     end
-    # end
-    #
-    #
-    # @show pareto_set
-    # @show length(pareto_set)
-    # @show max_index
-    # @show max_distance
-
-
-    # find the point with largest gap
-    # for i=1:length(all)
-    # end
-
     return x_L
 end
 
+"""
+    get_ref(points::Vector{B_points})::Int
+
+get the reference point for L>2
+"""
+function get_ref(points::Vector{B_points})::Int
+    dis(x::Int) = LinearAlgebra.norm(points[x].cost - points[x-1].cost)^2+LinearAlgebra.norm(points[x].cost - points[x+1].cost)^2
+    max_index = 2 #point with largest distance to its adjacent points
+    max_distance=0.
+
+    for i = 2:length(points)-1
+        if dis(i)/(points[i].weight+1) >= max_distance
+            max_distance = dis(i)
+            max_index = i
+        end
+    end
+
+    return max_index
+end
 
 """
     Optimize_Bi!(p::DSProblem)
@@ -213,21 +178,53 @@ function Optimize_Bi!(p::DSProblem)
     # Initialization
     undominated_points = pareto_front(initial_X_L(p1, p2))
     display(undominated_points)
+
+    ref_point=Vector{Float64}()
+    L=length(undominated_points)
+    j = 0     #initial point for the refomulated sigle-obj problem
+    δ = 0.0 # quantify the quality of the coverage of the undominated points
+    # here is the coverage of the point with largest distance to its adjacent points
+
     # Main iteration
-    δ = 0.0 #measure the coverage
-    w = 0 #weight
-    # ref_point, initial_point = get_ref_init(p1, p2)
+    while 1
+        if L==1
+            j=1
+        elseif L==2
+            j=2
+            ref_point=[undominated_points[2].cost[1],undominated_points[1].cost[2]]
+            δ=LinearAlgebra.norm(points[x].cost - points[x-1].cost)^2
+        else
 
-    # p_re=DSProblem()
-    # while 1
-    #     ref_point, initial_point=get_ref_init(p1,p2)
-    #     display(ref_point)
-    #
-    #     p.full_output && OutputIterationDetails(p_re)
-    #     # OptimizeLoop(p_re)
-    #     w+=1
-    # end
+        end
 
+        #Reference point determination
+        j = get_ref(undominated_points)
+        @show j
+
+
+
+        #
+        # @show pareto_set
+        # @show length(pareto_set)
+        # @show max_index
+        # @show max_distance
+
+        # p_re=DSProblem()
+        # while 1
+        #     ref_point, initial_point=get_ref_init(p1,p2)
+        #     display(ref_point)
+        #
+        #     p.full_output && OutputIterationDetails(p_re)
+        #     # OptimizeLoop(p_re)
+        #     w+=1
+        # end
+
+        # #update X_L
+        # # push sth
+        # sort!(points, by = v -> v.cost, rev = false)
+        # undominated_points[j].weight+=1
+        break
+    end
 
     println("Finish")
 end
